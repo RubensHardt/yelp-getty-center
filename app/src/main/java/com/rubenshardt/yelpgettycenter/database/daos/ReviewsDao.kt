97114e8822db00f9ab4bcb1ea15abda547b8c801
Rubens Hardt Junior <rubenshardtster@gmail.com>
2019-03-10 18:53:49 -0300

    Add the reviews dao

---
+++ app/src/main/java/com/rubenshardt/yelpgettycenter/database/daos/ReviewsDao.kt
@@ -0,0 +1,18 @@
+package com.rubenshardt.yelpgettycenter.database.daos
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import com.rubenshardt.yelpgettycenter.model.reviews.Review
+
+@Dao
+interface ReviewsDao {
+
+    @Query("SELECT * FROM reviews")
+    fun getReviews(): LiveData<List<Review>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertReviews(reviews: List<Review>)
+}
