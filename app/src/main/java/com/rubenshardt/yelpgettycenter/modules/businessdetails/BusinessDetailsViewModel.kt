908ac95262921a473ff3325f0d229978b41bf2a8
Rubens Hardt Junior <rubenshardtster@gmail.com>
2019-03-24 13:40:20 -0300

    Update business viewmodel according with the changes in the repository

--- app/src/main/java/com/rubenshardt/yelpgettycenter/modules/businessdetails/BusinessDetailsViewModel.kt
+++ app/src/main/java/com/rubenshardt/yelpgettycenter/modules/businessdetails/BusinessDetailsViewModel.kt
@@ -1,8 +1,9 @@
 package com.rubenshardt.yelpgettycenter.modules.businessdetails
 
-import android.arch.lifecycle.MediatorLiveData
+import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.rubenshardt.yelpgettycenter.model.business.Business
 import com.rubenshardt.yelpgettycenter.model.business.Coordinates
 import com.rubenshardt.yelpgettycenter.utils.helpers.SingleLiveEvent
 import com.rubenshardt.yelpgettycenter.utils.repositories.YelpRepository
@@ -12,9 +13,8 @@ class BusinessDetailsViewModel(
     private val yelpRepository: YelpRepositoryInterface = YelpRepository
 ) : ViewModel(), BusinessDetailsInterface {
 
-    override var businessLiveData = yelpRepository.businessLiveData
-    override var reviewsLiveData = yelpRepository.reviewsLiveData
-    override val refreshingLiveData = MediatorLiveData<Boolean>()
+    override lateinit var businessLiveData: LiveData<Business>
+    override val refreshingLiveData = MutableLiveData<Boolean>()
     override val loadingLiveData = MutableLiveData<Boolean>()
     override val errorLiveEvent = SingleLiveEvent<String>()
     override val coordinatesLiveEvent = SingleLiveEvent<Coordinates>()
@@ -24,26 +24,18 @@ class BusinessDetailsViewModel(
     override val photoUrlLiveEvent = SingleLiveEvent<String>()
 
     init {
-        //Shows the loading if it's loading the business for the first time, if not, shows the refreshing loading
-        refreshingLiveData.addSource(yelpRepository.loadingBusinessLiveData) { isBusinessLoading ->
-            val isLoading = isBusinessLoading ?: false
-            val hasLoadedValues = businessLiveData.value != null
-            loadingLiveData.postValue(isLoading && !hasLoadedValues)
-            refreshingLiveData.postValue(isLoading && hasLoadedValues)
-        }
-        //Shows the refreshing loading if the business was already fetched, otherwise shows nothing
-        refreshingLiveData.addSource(yelpRepository.loadingReviewsLiveData) { areReviewsLoading ->
-            val isLoading = areReviewsLoading ?: false
-            val hasLoadedValues = businessLiveData.value != null
-            if (isLoading && hasLoadedValues) {
-                refreshingLiveData.postValue(true)
-            }
-        }
+        loadingLiveData.postValue(true)
+        businessLiveData = yelpRepository.getBusiness(onError = ::onError)
     }
 
     override fun refreshBusinessDetails() {
-        yelpRepository.refreshBusiness(onError = ::onError)
-        yelpRepository.refreshReviews(onError = ::onError)
+        refreshingLiveData.postValue(true)
+        yelpRepository.refreshBusiness(
+            onSuccess = {
+                refreshingLiveData.postValue(false)
+            },
+            onError = ::onError
+        )
     }
 
     override fun onMapClicked(coordinates: Coordinates) {
